d84b513c725a6bb203ca48158f2cf6b804260379
Tiny Steps <yohannessharew55@gmail.com>
2023-01-02 19:52:52 -0300

    news

--- Event_Tracker/settings.py
+++ Event_Tracker/settings.py
@@ -119,3 +119,4 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
+CSRF_TRUSTED_ORIGINS = ['*']
